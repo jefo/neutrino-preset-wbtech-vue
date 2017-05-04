beee2000a580457f33868fb34b8357d795ebe418
jefo <johnyjefo@gmail.com>
2017-05-04 11:38:08 +0300

    :rocket: add karma preset

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const airbnb = require('neutrino-preset-airbnb-base');
 const web = require('neutrino-preset-web');
 const vue = require('neutrino-preset-vue');
+const karma = require('neutrino-preset-karma');
 const override = require('./internal/neutrino/override.js');
 
 module.exports = () => {
   neutrino.use(airbnb);
   neutrino.use(web);
   neutrino.use(vue);
+  neutrino.use(karma);
   neutrino.config.publicPath('/static/build');
   if (override) {
     neutrino.use(override);
